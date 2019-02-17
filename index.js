30a2eaa7b373bd7b099c0b08cb063d2b31ccd554
Shlomi Nissan <shlomi@betamark.com>
2019-02-16 18:18:40 -0800

    Add fetch wrapper with retries

--- index.js
+++ index.js
@@ -1,3 +1,37 @@
-module.exports = function(url, options) {
-  return fetch(url, options);
-};
+export default function(url, options) {
+    let retries = 3;
+    let delay = 3000;
+
+    if (options && "retries" in options) {
+        retries = options.retries;
+    }
+
+    if (options && "delay" in options) {
+        delay = options.delay;
+    }
+
+    return new Promise((resolve, reject) => {
+        const _fetch = async n => {
+            try {
+                let response = await fetch(url, options);
+                resolve(response);
+            }
+            catch(error) {
+                console.log(error);
+                if (n > 0) {
+                    retry(n);
+                } else {
+                    reject(error);
+                }
+            }
+        }
+
+        function retry(n) {
+            setTimeout(() => {
+                _fetch(--n);
+            }, delay);
+        }
+
+        _fetch(retries);
+    });
+}
